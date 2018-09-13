00d8a0da75cb2091b14414479019affafae645c7
TK800m <42999022+TK800m@users.noreply.github.com>
2018-09-13 14:19:18 +0200

    change delay

--- fastreading20.py
+++ fastreading20.py
@@ -7,7 +7,7 @@ Created on Tue Sep 11 11:46:09 2018
 
 import pygame, sys, time
 #variables needed. Delay, TXTsource and syllBonus most important inputs
-delay = 0.3
+delay = 0.4
 wordCount = 0
 BGcolor = (255,255,255)
 W = 900
