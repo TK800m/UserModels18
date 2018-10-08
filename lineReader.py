9cacb8c4333c461a665d5b03bdd589b43a608e60
lpraetorius <leonardpraetorius@gmail.com>
2018-10-08 19:44:13 +0200

    code for PsychoPy version

--- lineReader.py
+++ lineReader.py
@@ -1,59 +1,219 @@
-##printing lines
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This experiment was created using PsychoPy3 Experiment Builder (v3.0.0b10),
+    on Mon Oct  8 19:07:39 2018
+If you publish work using this script please cite the PsychoPy publications:
+    Peirce, JW (2007) PsychoPy - Psychophysics software in Python.
+        Journal of Neuroscience Methods, 162(1-2), 8-13.
+    Peirce, JW (2009) Generating stimuli for neuroscience using PsychoPy.
+        Frontiers in Neuroinformatics, 2:10. doi: 10.3389/neuro.11.010.2008
+"""
 
-import pygame, sys, time, math
-
-pygame.init()
-width = 800
-height = 600
-myfont = pygame.font.Font(None, 90)
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Fastreading with Eyetracker")
-## initialize texts
-texts = ["text1.txt", "text2.txt", "text3.txt", "text4.txt"]
-ntexts = len(texts)
-##add questions for texts
-FPS = 100
-BLACK = 0,0,0
-WHITE = 255,255,255
-clock = pygame.time.Clock()
-
-def showWord(word, locX):
-    text = myfont.render(word,1,(0,0,0))
-    screen.blit(text,(locX ,height/2))
-    pygame.display.update()
-    return locX
+from __future__ import absolute_import, division
+from psychopy import locale_setup, sound, gui, visual, core, data, event, logging, clock, parallel
+from psychopy.constants import (NOT_STARTED, STARTED, PLAYING, PAUSED,
+                                STOPPED, FINISHED, PRESSED, RELEASED, FOREVER)
+import numpy as np  # whole numpy lib is available, prepend 'np.'
+from numpy import (sin, cos, tan, log, log10, pi, average,
+                   sqrt, std, deg2rad, rad2deg, linspace, asarray)
+from numpy.random import random, randint, normal, shuffle
+import os  # handy system and path functions
+import sys  # to get file system encoding
 
 
-locX = 300
-box_dir = 3
- 
-while 1:
-  clock.tick(FPS)
- 
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      sys.exit()
-    if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    FPS += 5
-                if event.key == pygame.K_DOWN:
-                    if FPS >= 10:
-                    	FPS -= 5
-                if event.key == pygame.K_SPACE:
-                    time.sleep(10)
- 
-  screen.fill(WHITE)
- #box stuff
+# Ensure that relative paths start from the same directory as this script
+_thisDir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(_thisDir)
 
-  locX += box_dir
-  if locX >= 620:
-    locX = 620
-    box_dir = -3
-  elif locX <= 0:
-    locX = 0
-    box_dir = 3
+# Store info about the experiment session
+expName = 'lineReader'  # from the Builder filename that created this script
+expInfo = {'participant': '', 'session': '001'}
+dlg = gui.DlgFromDict(dictionary=expInfo, title=expName)
+if dlg.OK == False:
+    core.quit()  # user pressed cancel
+expInfo['date'] = data.getDateStr()  # add a simple timestamp
+expInfo['expName'] = expName
 
-  showWord("Here is a very long text that is longer than a full screen so I will have to work out how this works with a text", locX)
-  pygame.display.flip()
+# Data file name stem = absolute path + name; later add .psyexp, .csv, .log, etc
+filename = _thisDir + os.sep + u'data/%s_%s_%s' % (expInfo['participant'], expName, expInfo['date'])
 
+# An ExperimentHandler isn't essential but helps with data saving
+thisExp = data.ExperimentHandler(name=expName, version='',
+    extraInfo=expInfo, runtimeInfo=None,
+    originPath='/Users/leo/Desktop/lineReader.py',
+    savePickle=True, saveWideText=True,
+    dataFileName=filename)
+# save a log file for detail verbose info
+logFile = logging.LogFile(filename+'.log', level=logging.EXP)
+logging.console.setLevel(logging.WARNING)  # this outputs to the screen, not a file
 
+endExpNow = False  # flag for 'escape' or other condition => quit the exp
+
+# Start Code - component code to be run before the window creation
+
+# Setup the Window
+win = visual.Window(
+    size=[1366, 768], fullscr=True, screen=0,
+    allowGUI=True, allowStencil=False,
+    monitor='testMonitor', color=[0,0,0], colorSpace='rgb',
+    blendMode='avg', useFBO=True)
+# store frame rate of monitor if we can measure it
+expInfo['frameRate'] = win.getActualFrameRate()
+if expInfo['frameRate'] != None:
+    frameDur = 1.0 / round(expInfo['frameRate'])
+else:
+    frameDur = 1.0 / 60.0  # could not measure, so guess
+
+# Initialize components for Routine "trial"
+trialClock = core.Clock()
+Background = visual.ImageStim(
+    win=win, name='Background',
+    image='background2.png', mask=None,
+    ori=0, pos=(0, 0), size=(2, 2),
+    color=[1,1,1], colorSpace='rgb', opacity=1,
+    flipHoriz=False, flipVert=False,
+    texRes=128, interpolate=True, depth=0.0)
+Text = visual.TextStim(win=win, name='Text',
+    text='default text',
+    font='Arial',
+    pos=[0,0], height=0.3, wrapWidth=100, ori=0, 
+    color='black', colorSpace='rgb', opacity=1, 
+    languageStyle='LTR',
+    depth=-1.0);
+
+# Initialize components for Routine "Questions"
+QuestionsClock = core.Clock()
+rating = visual.RatingScale(win=win, name='rating', marker='triangle', size=1.0, pos=[0.0, -0.4], choices=["'A: conclusions that turned out to be scientifically invalid.'", "'B: a new appreciation of the benefits of classical music.'", "'C: over-excited reactions among people concerned with children.'", "'D: I don't know.'"], tickHeight=-1)
+
+# Create some handy timers
+globalClock = core.Clock()  # to track the time since experiment started
+routineTimer = core.CountdownTimer()  # to track time remaining of each (non-slip) routine 
+
+# ------Prepare to start Routine "trial"-------
+t = 0
+trialClock.reset()  # clock
+frameN = -1
+continueRoutine = True
+# update component parameters for each repeat
+# keep track of which components have finished
+trialComponents = [Background, Text]
+for thisComponent in trialComponents:
+    if hasattr(thisComponent, 'status'):
+        thisComponent.status = NOT_STARTED
+
+# -------Start Routine "trial"-------
+while continueRoutine:
+    # get current time
+    t = trialClock.getTime()
+    frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+    # update/draw components on each frame
+    
+    # *Background* updates
+    if t >= 0.0 and Background.status == NOT_STARTED:
+        # keep track of start time/frame for later
+        Background.tStart = t
+        Background.frameNStart = frameN  # exact frame index
+        Background.setAutoDraw(True)
+    if Background.status == STARTED and frameN >= 1800:
+        Background.setAutoDraw(False)
+    
+    # *Text* updates
+    if frameN >= 0 and Text.status == NOT_STARTED:
+        # keep track of start time/frame for later
+        Text.tStart = t
+        Text.frameNStart = frameN  # exact frame index
+        Text.setAutoDraw(True)
+    if Text.status == STARTED and frameN >= 1800:
+        Text.setAutoDraw(False)
+    if Text.status == STARTED:  # only update if drawing
+        speed = -0.05
+        Text.setPos((-0.05*frameN+47, 0), log=False)
+        Text.setText('Intro:  Presenter: In 1993 researchers in the United States discovered that students who listened to ten minutes of Mozart’s “Sonata for Two Pianos in D” significantly increased their IQ scores. Now on BBC radio 4 Paul Robertson, the leader of the Medici String Quartet explores the Mozart effect. (1) Paul Robertson: So this is it. This is the music which makes you brainy. Stick with us for just ten minutes and it will change your life. Well, That’s what the media would have us believe when they invented the term “Mozart effect”. It sent the world into a spin. Shops sold out of this recording. The State of Georgia called for a hundred and five thousand dollars to be set aside so that every mother could receive a free classical music tape, and parents all over the UK jumped at the chance to get their children ahead of the school pack. Mozart had apparently emerged from more than two hundred years of benign obscurity to become the greatest influence on health and education the world has ever known ', log=False)
+    
+    # check if all components have finished
+    if not continueRoutine:  # a component has requested a forced-end of Routine
+        break
+    continueRoutine = False  # will revert to True if at least one component still running
+    for thisComponent in trialComponents:
+        if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+            continueRoutine = True
+            break  # at least one component has not yet finished
+    
+    # check for quit (the Esc key)
+    if endExpNow or event.getKeys(keyList=["escape"]):
+        core.quit()
+    
+    # refresh the screen
+    if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+        win.flip()
+
+# -------Ending Routine "trial"-------
+for thisComponent in trialComponents:
+    if hasattr(thisComponent, "setAutoDraw"):
+        thisComponent.setAutoDraw(False)
+# the Routine "trial" was not non-slip safe, so reset the non-slip timer
+routineTimer.reset()
+
+# ------Prepare to start Routine "Questions"-------
+t = 0
+QuestionsClock.reset()  # clock
+frameN = -1
+continueRoutine = True
+# update component parameters for each repeat
+rating.reset()
+# keep track of which components have finished
+QuestionsComponents = [rating]
+for thisComponent in QuestionsComponents:
+    if hasattr(thisComponent, 'status'):
+        thisComponent.status = NOT_STARTED
+
+# -------Start Routine "Questions"-------
+while continueRoutine:
+    # get current time
+    t = QuestionsClock.getTime()
+    frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+    # update/draw components on each frame
+    # *rating* updates
+    if t >= 0.0 and rating.status == NOT_STARTED:
+        # keep track of start time/frame for later
+        rating.tStart = t
+        rating.frameNStart = frameN  # exact frame index
+        rating.setAutoDraw(True)
+    continueRoutine &= rating.noResponse  # a response ends the trial
+    
+    # check if all components have finished
+    if not continueRoutine:  # a component has requested a forced-end of Routine
+        break
+    continueRoutine = False  # will revert to True if at least one component still running
+    for thisComponent in QuestionsComponents:
+        if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+            continueRoutine = True
+            break  # at least one component has not yet finished
+    
+    # check for quit (the Esc key)
+    if endExpNow or event.getKeys(keyList=["escape"]):
+        core.quit()
+    
+    # refresh the screen
+    if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+        win.flip()
+
+# -------Ending Routine "Questions"-------
+for thisComponent in QuestionsComponents:
+    if hasattr(thisComponent, "setAutoDraw"):
+        thisComponent.setAutoDraw(False)
+# store data for thisExp (ExperimentHandler)
+thisExp.addData('rating.response', rating.getRating())
+thisExp.addData('rating.rt', rating.getRT())
+thisExp.nextEntry()
+# the Routine "Questions" was not non-slip safe, so reset the non-slip timer
+routineTimer.reset()
+# these shouldn't be strictly necessary (should auto-save)
+thisExp.saveAsWideText(filename+'.csv')
+thisExp.saveAsPickle(filename)
+logging.flush()
+# make sure everything is closed down
+thisExp.abort()  # or data files will save again on exit
+win.close()
+core.quit()
